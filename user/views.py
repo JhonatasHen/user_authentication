7f7bf25ffa0c8da0050d8c12dc21151ec369ca5f
Jhonatas Henrique Pereira <jhonatas.hen.dev@gmail.com>
2023-07-20 01:10:18 -0300

    first commit

---
+++ user/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            return HttpResponse('Já existe um usuário com esse username.')
+
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        user.save()
+        return HttpResponse('Usuário cadastrado com sucesso!')
+
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=username, password=senha)
+
+        if user:
+            login(request, user)
+            return HttpResponse('Autenticado')
+        
+        else:
+            return HttpResponse('Usuário ou senha inválido')
+
+def plataforma(request):
+    ...
